4bc8f3a1aa90123cebb30d7c109d35d91ab91a59
cedric bazillou <cedricbazillou@gmail.com>
2016-11-14 19:51:18 -0600

    Add more class to illustrate skin IO techniques
    comment out PointWeights class  in order to finish blog article

--- skinUtils.py
+++ skinUtils.py
@@ -14,6 +14,202 @@ import uuid
 import zipfile
 
 
+class SkinSettings(object):
+    def __init__(self, skinDeformer):
+        self.deformerName = None
+        self.shape = None
+        self.shapePath = None
+        self.influences = []
+
+        self.skinningMethod = 0
+        self.normalizeWeights = False
+        self.abcWeightsFile = None
+
+        self.processingTime = 0
+        self.report = ''
+        self.skinDeformer = skinDeformer
+
+        self.getSkinSettings()
+
+    def getSkinSettings(self):
+        self.deformerName = self.skinDeformer
+        self.influences = maya.cmds.skinCluster(self.skinDeformer,
+                                                q=True,
+                                                inf=True)
+
+        self.skinningMethod = maya.cmds.getAttr('{}.skinningMethod'.format(self.skinDeformer))
+        self.normalizeWeights = maya.cmds.getAttr('{}.normalizeWeights'.format(self.skinDeformer))
+
+        self.shape = maya.cmds.skinCluster(self.skinDeformer,
+                                           q=True,
+                                           geometry=True)[0]
+
+    def toJson(self):
+        return json.dumps(self, 
+                          cls=ObjectEncoder, 
+                          indent=2, 
+                          sort_keys=True)
+
+
+class ShapeSettings(object):
+    def __init__(self, shape):
+        self.shape = shape
+        self.shapePath = maya.OpenMaya.MDagPath()
+        self.transform = None
+        self.pointCount = 0
+        self.shapeType = ''
+
+        self.uCount = 0
+        self.vCount = 0
+        self.wCount = 0
+
+        self.getShapeSettings()
+
+    def getMObject(self, nodeName):
+        selList = maya.OpenMaya.MSelectionList()
+        maya.OpenMaya.MGlobal.getSelectionListByName(nodeName, 
+                                                     selList)
+        depNode = maya.OpenMaya.MObject()
+        selList.getDependNode(0, depNode) 
+
+        return depNode
+
+    def getShapeSettings(self):
+        self.transform = maya.cmds.listRelatives(self.shape, p=True)[0]
+        shapeApiObject = self.getMObject(self.shape)
+
+        maya.OpenMaya.MDagPath.getAPathTo(shapeApiObject,
+                                          self.shapePath)    
+
+        pointCount = 0
+        simpleShapeType = ['mesh', 'nurbsCurve']
+        self.shapeType = maya.cmds.nodeType(self.shape)
+
+        if self.shapeType in simpleShapeType:
+            componentType = maya.OpenMaya.MFn.kMeshVertComponent
+
+            if self.shapeType == 'mesh':
+                shapeFunctionUtils = maya.OpenMaya.MFnMesh(self.shapePath)
+                self.pointCount = shapeFunctionUtils.numVertices()
+
+            elif self.shapeType == 'nurbsCurve':
+                shapeFunctionUtils = maya.OpenMaya.MFnNurbsCurve(self.shapePath)
+                self.pointCount = shapeFunctionUtils.numVertices()
+
+            self.uCount = int(self.pointCount)
+
+        elif self.shapeType == 'nurbsSurface':
+            shapeFunctionUtils = maya.OpenMaya.MFnNurbsSurface(self.shapePath)
+
+            self.uCount = shapeFunctionUtils.numCVsInU()
+            self.wCount = shapeFunctionUtils.numCVsInV()
+
+            self.pointCount = shapeFunctionUtils.numCVsInU() * shapeFunctionUtils.numCVsInV()
+
+        elif self.shapeType == 'lattice':
+            componentType = maya.OpenMaya.MFn.kLatticeComponent
+            shapeFunctionUtils = maya.OpenMayaAnim.MFnLattice(self.shapePath)
+
+            sDivivisionParam = maya.OpenMaya.MScriptUtil()
+            sDivivisionParam.createFromInt(0)    
+            sDivivisionPtr = sDivivisionParam.asUintPtr()    
+
+            tDivivisionParam = maya.OpenMaya.MScriptUtil()
+            tDivivisionParam.createFromInt(0)    
+            tDivivisionPtr = tDivivisionParam.asUintPtr()  
+
+            uDivivisionParam = maya.OpenMaya.MScriptUtil()
+            uDivivisionParam.createFromInt(0)    
+            uDivivisionPtr = uDivivisionParam.asUintPtr()  
+
+            shapeFunctionUtils.getDivisions(sDivivisionPtr,
+                                            tDivivisionPtr,
+                                            uDivivisionPtr)
+
+            sDivivision = maya.OpenMaya.MScriptUtil(sDivivisionPtr).asInt()
+            tDivivision = maya.OpenMaya.MScriptUtil(tDivivisionPtr).asInt()
+            uDivivision = maya.OpenMaya.MScriptUtil(uDivivisionPtr).asInt()
+
+            self.uCount = sDivivision
+            self.vCount = tDivivision
+            self.wCount = uDivivision
+
+            self.pointCount = sDivivision * tDivivision * uDivivision
+
+    def getComponent(self, pointIndex):
+        simpleShapeType = ['mesh', 'nurbsCurve']
+        if self.shapeType in simpleShapeType:
+            return '{shape}.vtx[{pointIndex}]'.format(shape=self.transform,
+                                                      pointIndex=pointIndex)
+
+        elif self.shapeType == 'nurbsSurface':
+            targetRow = pointIndex / self.vCount
+            targetColumn = pointIndex - targetRow
+            return '{shape}.cv[{row1}][{row2}]'.format(shape=self.transform,
+                                                       row1=targetRow,
+                                                       row2=targetColumn)
+
+        elif self.shapeType == 'lattice':
+            heightRatio = (self.uCount*self.vCount)
+            targetHeight = pointIndex / heightRatio
+
+            heightLevel = heightRatio*targetHeight
+            rowLevel = (heightLevel - targetHeight)
+
+            #print targetHeight, heightRatio, heightLevel, rowLevel, pointIndex
+
+            targetRow = pointIndex / self.vCount
+            targetColumn = pointIndex - targetRow
+            return '{shape}.pt[{row1}][{row2}][{row3}]'.format(shape=self.transform,
+                                                               row1=targetColumn,
+                                                               row2=targetRow,
+                                                               row3=targetHeight)
+
+"""
+class PointWeights(object):
+    def __init__(self):
+        self.timeProcessing = TimeProcessor()
+
+    def importWeights(self, inputMesh):
+        pass
+
+    def getWeights(self, 
+                   inputMesh, 
+                   skinSettings,
+                   shapeSettings):
+        for pointIndex in xrange(shapeSettings.pointCount):
+            component = self.getComponent(pointIndex, shapeSettings)
+
+            rawWeights = maya.cmds.skinPercent(component,
+                                               skinSettings.skinDeformer,
+                                               q=True,
+                                               value=True)
+
+            return {weightIndex:weight \
+                    for weightIndex, weight \
+                    in enumerate(rawWeights) if weight > 0.0}
+
+    def saveWeights(self,
+                    inputMesh, 
+                    targetDirectory):
+        targetFile = os.path.join(targetDirectory, '{0}.wgt'.format(inputMesh))
+
+        skinNode = maya.mel.eval('findRelatedSkinCluster {}'.format(inputMesh))
+        if skinNode is None:
+            return
+
+        skinSettings = SkinSettings(skinNode)
+        shapeSettings = ShapeSettings(skinSettings.shape)
+
+        with open(targetFile, 'w') as skinFile:
+            for pointIndex in xrange(shapeSettings.pointCount):
+                skinData = self.getWeights(inputMesh, 
+                                           skinSettings,
+                                           shapeSettings):
+                skinFile.write(skinData)
+
+"""
+
 class AsciiInjection(object):
     def __init__(self):
         self.timeProcessing = TimeProcessor()
@@ -287,26 +483,6 @@ class ObjectEncoder(json.JSONEncoder):
                             )
             return self.default(jsonDict)
         return inputObject
-
-
-class SkinSettings(object):
-    def __init__(self):
-        self.deformerName = None
-        self.shape  = None
-        self.influences = []
-
-        self.skinningMethod = 0
-        self.normalizeWeights = False
-        self.abcWeightsFile = None
-
-        self.processingTime = 0
-        self.report = ''
-
-    def toJson(self):
-        return json.dumps(self, 
-                          cls=ObjectEncoder, 
-                          indent=2, 
-                          sort_keys=True)
 
 
 class SkinSet(object):
@@ -650,16 +826,6 @@ class SkinIO(object):
 
         return list(set(outputSkins))
 
-    def getSkinSettings(self, skinDeformer):
-        skinSettings = SkinSettings()
-
-        skinSettings.deformerName = skinDeformer
-        skinSettings.influences = maya.cmds.skinCluster(skinDeformer, q=True, inf=True)
-        skinSettings.skinningMethod = maya.cmds.getAttr('{}.skinningMethod'.format(skinDeformer))
-        skinSettings.normalizeWeights = maya.cmds.getAttr('{}.normalizeWeights'.format(skinDeformer))
-
-        return skinSettings
-
     def extractSettings(self, inputSkinSettings):
         inputSkinSettings.report = ''
         jsonData = inputSkinSettings.toJson()
@@ -697,7 +863,7 @@ class SkinIO(object):
             return []
 
         for inputSkinNode in inputSkinNodes:
-            skinSettings = self.getSkinSettings(inputSkinNode)
+            skinSettings = SkinSettings(inputSkinNode)
             skinSettings.shape = maya.cmds.listRelatives(inputTransform,
                                                          s=True,
                                                          fullPath=True)[0]
